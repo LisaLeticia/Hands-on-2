cd9dbe3c0e1bf5fe5740f037bcd28b8e5418dfd4
YOSE <143077820+yos564@users.noreply.github.com>
2024-04-24 23:51:45 +0800

    Update cotask1.py

--- cotask1.py
+++ cotask1.py
@@ -5,7 +5,7 @@ with open("ke_yi_raw.txt", "r", encoding="utf-8") as f:
 
 def eliminate(S):
     a = re.compile(r'more')
-    result = [a.sub('\n', x) for x in S]  #[] creates a new list for tha string eliminated all the "more"
+    result = [a.sub('\t', x) for x in S]  #[] creates a new list for tha string eliminated all the "more"
     #sub() method of a compiled regular expression to replace occurrences of the pattern specified in the regular expression with the replacement string
     return result
 
