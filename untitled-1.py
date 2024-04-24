0e9a2f21b5e0d964984b76db19a924c256561b6c
LisaLeticia <160850149+LisaLeticia@users.noreply.github.com>
2024-04-24 15:56:35 +0800

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -1,3 +1,25 @@
 with open("ke_yi_raw.txt", "r", encoding = "utf-8") as f:
     List = f.readlines()
-    print(List)
+    #print(List)
+    
+    
+for i in range(len(List)):
+    
+    #先用more, more當關鍵字合併為一句string，做成新List
+    if "more"[0] in List:
+        string = 
+        
+    
+    #新List若有"可以"，則根據前後標點符號切出要的句子
+    #句子中\n, \t刪掉
+    
+    #每句換行
+    #寫出生成.txt
+    
+    
+    
+
+#if "可以" in List:
+     #   x = (List.split("。")[1]).split["。"][0]
+      #  print(x)
+    
