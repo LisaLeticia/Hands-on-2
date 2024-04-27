0f66805a175a7157b8185a5739c567e10944d2f8
emilyganpeijie <ganpeijie3@gmail.com>
2024-04-27 23:36:37 +0800

    update output format

--- em_attempt.py
+++ em_attempt.py
@@ -7,12 +7,10 @@ list1 = re.sub("\n|more|\t", '', str(raw_data))     #removing \n, \t, and "more"
 
 pattern = re.compile(r'\w*可以\w*')       #output pattern
 matches = pattern.finditer(list1)
-output_list = []        #initializing output_list
+#output_list = []        #initializing output_list
 
 for i in matches:       #filling output_list with matched lists
-    output_list.append(i.group(0))
-print(output_list)
-
-with open("ke_yi_processed.txt", "w") as f:     #writing the output_list into a txt file named "ke_yi_processed.txt"
-    print(output_list, file=f)
+    #output_list.append(i.group(0))
+    with open("ke_yi_processed.txt", "a") as f:     #writing the output_list into a txt file named "ke_yi_processed.txt"
+        print(i.group(0), file=f)
     
