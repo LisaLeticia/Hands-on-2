adc6ceab58059418b135ee3f5f2634b8cc634b1e
LisaLeticia <160850149+LisaLeticia@users.noreply.github.com>
2024-04-27 22:41:21 +0800

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -1,19 +1,40 @@
-with open("ke_yi_raw.txt", "r", encoding = "utf-8") as f:
+with open("ke_yi_raw.txt", "r", encoding = "utf-8") as f:    #讀檔
     List = f.readlines()
-    #print(List)
-    
-    
+    List = List[1:]                      #刪除第一個字串，避免後面合併錯誤
+    #print(List)    
+
 
 import re
 a = re.compile(r"more")
-List1 = a.split(str(List))
-print(List1)                        #先用more, more當關鍵字合併為一句string，做成新List
-    
-    
-for i in range(len(List1)):
+List = a.split(str(List))          #先用more, more當關鍵字合併為一句string，做成新List
+#print(List)                        
 
-    if "可以" in List1:            #新List若有"可以"，則根據前後標點符號切出要的句子
-     #   string = 
+
+keywords = ["'", "\\", ",", "t", "[", "n", " "]     #刪除影響合併的冗詞
+str_list = List
+new_list = []
+for string in str_list:
+    for keyword in keywords:
+        string = string.replace(keyword, "")
+    new_list.append(string)
+new_list = [s for s in new_list if s.strip()]     #過濾空字串
+print(new_list)                                   #得到的新清單
+
+
+#for i in range(len(List)):
+
+#i = 1
+#while i < len(List):
+#    if "可以" in List[i]:       #若"可以"在句中，與前一字串合併
+#        List[i-1] += List[i]
+#        del List[i]
+#    else:
+#        del List[i]
+#print(List)
+        
+        
+        #新List若有"可以"，則根據前後標點符號切出要的句子
+         
         
     
     
